1cf4429b82a7c11ff97cb71f7d5b665d72ea4734
PhilTech-pixel <njoromanga49@gmail.com>
2025-07-12 10:55:07 +0300

    Delete Function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import NavBar from "./components/NavBar.jsx";
-import {SquarePen, Trash2} from "lucide-react";
+
+import TodoCard from "./components/TodoCard.jsx";
+import {Toaster} from "react-hot-toast";
 
 const App = () => {
     const [todo, setTodo] = useState([])
@@ -15,22 +17,16 @@ const App = () => {
         }
         fetchTodo()
     }, []);
+
+
     return (
         <>
+            <Toaster/>
         <NavBar/>
         <div className="flex flex-wrap">
         {todo.map((todo)=>{
             return(
-                <div key={todo._id} className="card w-96 bg-base-100 shadow-xl ml-5 mt-5">
-                    <div className="card-body">
-                        <h2 className="card-title">{todo.title}</h2>
-                        <p>{todo.description}</p>
-                        <div className="card-actions justify-end">
-                            <SquarePen className="text-white" />
-                            <Trash2 className="text-red-500" />
-                        </div>
-                    </div>
-                </div>
+                <TodoCard todo={todo}/>
             )
             })}
 
---
+++ frontend/src/components/TodoCard.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { SquarePen, Trash2} from "lucide-react";
+import {Link} from 'react-router'
+import axios from "axios";
+import toast from "react-hot-toast";
+
+const TodoCard = ({todo}) => {
+
+    const handleDelete = async (e, id)=>{
+        e.preventDefault()
+        try {
+            if(!window.confirm("Are you sure you want to delete this task")) return;
+            await axios.delete(`http://localhost:5001/api/v1/todo/${id}`)
+            toast.success("Task Deleted Successfully")
+
+        }catch (e) {
+            toast.error("Error! Task not Deleted")
+        }
+    }
+    return (
+        <div className="card w-96 bg-base-100 shadow-xl ml-5 mt-5">
+            <div className="card-body">
+                <h2 className="card-title">{todo.title}</h2>
+                <p>{todo.description}</p>
+                <div className="card-actions justify-end">
+                    <SquarePen className="text-white" />
+
+                    <button onClick={(e)=>handleDelete(e, todo._id)} >
+                        <Trash2 className="text-red-500" />
+                    </button>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TodoCard;

8ab67295db180ad5054d74218c080705d85adfcd
PhilTech-pixel <njoromanga49@gmail.com>
2025-07-13 17:44:49 +0300

    Links Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
-
+import React from 'react';
 import HomePage from "./pages/HomePage.jsx";
 import {Routes, Route} from "react-router";
 import CreatePage from "./pages/CreatePage.jsx";
 import EditPage from "./pages/EditPage.jsx";
+import {Toaster} from "react-hot-toast";
 
 const App = () => {
 
     return (
         <div>
+            <Toaster/>
             <Routes>
                 <Route path="/" element={<HomePage/>}/>
                 <Route path="/todo/create" element={<CreatePage/>}/>
--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,10 +1,72 @@
-import React from 'react';
+import React, {useState} from 'react';
+import toast from "react-hot-toast";
+import {useNavigate} from "react-router";
+import axios from "axios";
+import NavBar from "../components/NavBar.jsx";
+
 
 const CreatePage = () => {
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+
+    const [saving, setSaving] = useState(false)
+    const navigate = useNavigate()
+
+
+    const handleSubmit = async(e)=>{
+        e.preventDefault()
+        if(!title.trim() || !description.trim()){
+            toast.error("All fields are required")
+            return
+        }
+        setSaving(true)
+
+
+        try {
+
+            await axios.post("http://localhost:5001/api/v1/todo/", {
+                title,
+                description,
+            })
+            toast.success("Task Added Successfully")
+            navigate("/")
+        }catch (error) {
+            toast.error("Task not Created")
+        }finally {
+            setSaving(false)
+        }
+    }
     return (
-        <div className="min-h-screen bg-base-200 flex items-center justify-center">
-            
+        <>
+            <NavBar/>
+        <div className="min-h-screen flex items-center justify-center">
+
+            <div className="card max-w-96 bg-base-200 card-lg">
+                <div className="card-body m-5">
+
+                    <div className="card-title items-center justify-center">
+                        <h1>Add a New Note</h1>
+                    </div>
+                    <form onSubmit={handleSubmit}>
+                    <div className="form-control flex items-center justify-center">
+                        <input type="text" placeholder="Insert Title" className="input input-lg mt-5 mb-5"
+                            onChange={(e)=>setTitle(e.target.value)}
+                        />
+                    </div>
+                    <div className="form-control items-center justify-center">
+                        <textarea placeholder="Insert Description" className="textarea textarea-lg  max-w-xs "
+                            onChange={(e)=>setDescription(e.target.value)}
+                        ></textarea>
+
+                    </div>
+                    <div className="card-actions mt-5 justify-end">
+                        <button type="submit" className="btn btn-success" >Save</button>
+                    </div>
+                    </form>
+                </div>
+            </div>
         </div>
+        </>
     );
 };
 
